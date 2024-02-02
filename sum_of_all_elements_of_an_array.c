1c4e894066085f32b631b23c951cb688f5ecbc6d
Bhavanamedikonda <157686177+Bhavanamedikonda@users.noreply.github.com>
2024-02-02 22:43:31 +0530

    Add files via upload

---
+++ sum_of_all_elements_of_an_array.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+	int n,sum=0;
+	int i;
+	printf("enter the number of elements ");
+	scanf("%d",&n);
+	int array[n];
+	printf("enter %d elements",n);
+	for(i=0;i<n;i++)
+		scanf("%d",&array[i]);
+	
+	for(i=0;i<n;i++){
+		sum +=array[i];
+	}
+	printf("sum of elements %d",sum);
+	return 0;
+	
+	
+	
+}
